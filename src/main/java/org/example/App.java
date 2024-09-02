3c46143a82e6496c40b9d593eafaa47cdef2a14c
Vilhelm <vilhelm.kennedal@iths.se>
2024-09-02 13:52:41 +0200

    Meny layout

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,38 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        Scanner sc = new Scanner(System.in);
+
+        String menu = """
+                Elpriser
+                ========
+                1. Inmatning
+                2. Min, Max och Medel
+                3. Sortera
+                4. Bästa Laddningstid (4h)
+                e. Avsluta
+                """;
+        System.out.println(menu);
+        String input = sc.nextLine();
+
+
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
