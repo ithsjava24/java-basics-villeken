0ecb7f257afb0e02d255bb2dce7beb8ae9ed5d34
Vilhelm <vilhelm.kennedal@iths.se>
2024-09-16 15:37:29 +0200

    Final commit v2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,9 +6,6 @@ import java.util.Scanner;
 
 public class App {
 
-
-    public static final int ROW_COUNT = 10;
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
